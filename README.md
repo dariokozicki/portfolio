71cd3c482641345484c719e4ec3a6344a9230625
Dario <dariokozicki@gmail.com>
2020-03-24 18:57:25 -0300

    feat(lang,alert,icon): added fill-in text, alert to notify of responsiveness, changed bug icon

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Portfolio personal
 
+
+![Uptime Robot status](https://img.shields.io/uptimerobot/status/m784608547-d0fa4206097c6fe015698d83) [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/) ![Uptime Robot ratio (7 days)](https://img.shields.io/uptimerobot/ratio/7/m784608547-d0fa4206097c6fe015698d83) [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) 
+
 En este repositorio vas a encontrar proyectos previos y actuales, además de información personal y diversos documentos como Curriculum Vitae, entre otros.
 
-[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+El sitio no usa ningún framework ni preprocesador, en su lugar se realizó sólo con HTML5, CSS y Javascript para ir aprendiendo cómo funcionan las clases, animaciones y variables de CSS.  
 
-[Portfolio desplegado](https://dariokozicki.github.io/portfolio/)
+El objectivo es brindar una página personalizada que muestre las cosas que voy haciendo, sirviendo a su vez como una muestra en sí misma de lo que hago.
 
+
+[Hacé click acá para verlo en tu navegador!](https://dariokozicki.github.io/portfolio/)
+
+## English
+
+In this respository you'll find previous and current projects of mine, asides from personal information and multiple documents, such as my Curriculum Vitae. 
+
+The site doesn't make use of any frameworks or preprocessors; instead, it seizes utilities from HTML5, CSS, and JS. The purpose of this was to learn how classes, animations and variables are constructed within CSS files.
+
+The objective of this site is to provide a customized page to display the things I work on, as well as serve as an example of what I do.
+
+[Click here to see it online!](https://dariokozicki.github.io/portfolio/)
--- js/lang.js
+++ js/lang.js
@@ -19,10 +19,13 @@ const translations = {
                 "personal-portfolio": "Personal Portfolio",
                 "light-mode": "Light Mode",
                 "dark-mode": "Dark Mode",
-                "p1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac cursus sapien. Aliquam id ullamcorper erat, tempor luctus nibh. Sed at arcu lectus. Nullam viverra eu nibh quis sagittis. Nullam lobortis velit vel ligula pretium cursus. Praesent eget faucibus urna. Praesent lacus metus, accumsan a venenatis lobortis, tincidunt in augue.",
-                "p2": "Proin ac erat non dui fringilla finibus. Phasellus vel auctor metus. Phasellus et odio pretium, consectetur erat in, mollis lectus. Aliquam vel justo ac eros facilisis varius. In rutrum, tortor eu mollis vulputate, ante arcu accumsan enim, et placerat velit risus sed enim. Nam eu lorem at magna facilisis rutrum. Quisque sed scelerisque lacus.",
-                "p3": "Praesent efficitur metus a massa sodales sollicitudin. Aenean quam nunc, venenatis sed lacus quis, consequat feugiat nibh. Duis sed dictum felis. Ut nulla enim, feugiat vitae dui eu, consequat condimentum ipsum. Maecenas nec sagittis lectus, id mattis tellus. Pellentesque congue cursus lacus in tincidunt. Vivamus vel ornare mi, scelerisque pellentesque turpis. ",
-                "p4": "Donec feugiat, erat a luctus pretium, quam quam vulputate sapien, sit amet consequat ipsum leo vel nisi. Aenean imperdiet massa leo, in pellentesque est cursus sed. Donec pretium eget augue at lacinia. Etiam ultrices, purus nec vehicula porttitor, purus justo convallis sapien, ut volutpat dolor risus in quam. Proin felis felis, convallis sed convallis non, tristique vitae ex. "
+                "p1": "My name is Darío. I'm a developer and a Systems Engineer student living in Buenos Aires, Argentina. Designing and implementing solutions to other people's needs is my profession. I'm interested in full-time jobs working remotely or mixed.",
+                "p2": "I provide value for the company I work for by focusing on understanding the domain, modeling and documenting, implementing, and developing automated tests: saving both the company and I of valuable time and resources. Sharing ideas is important to me; that's why I believe working while exchanging opinions is the way to go. Working with others allows me to learn and apply myself.",
+                "p3": "Backend is my strength; I make robust models that satisfy the needs of today and scale tomorrow. Java is my main language, making use of Spring, Spark, Hibernate, Gson and Handlebars. I can also work with JDBC, making queries against (T- / PL- / ) SQL. I provide and consume APIs REST in my projects, although I have used SOAP. OOP and Functional programming are my favorite paradigms, combining design patterns and lambda expressions in my day to day.",
+                "p4": "I'm learning bit by bit front-end, and this site is an excuse to keep on learning. It doesn't contain any framework because I set myself up to learn the CSS fundamentals and keep it flexible. I do well on HTML5, CSS, Javascript, and I often make use of libraries such as jQuery, FontAwesome, i18next, and Handlebars for templating. If you'd like to see the source code, you're welcome to search through ",
+                "alert-responsive": "This site is responsive. Use it on any devices!",
+                "hey": "Hey!",
+                "repo": "my repository."
             }
         },
         es: {
@@ -41,10 +44,13 @@ const translations = {
                 "solutions":"Soluciones a medida.",
                 "light-mode": "Modo Claro",
                 "dark-mode": "Modo Oscuro",
-                "p1": "Nam non rhoncus sem. Mauris scelerisque non felis in condimentum. In malesuada quam in arcu mattis tincidunt. Suspendisse potenti. Nam congue, tellus vel porttitor ornare, leo nisl molestie ipsum, nec mattis tellus nunc vitae enim. Curabitur placerat vulputate tortor vel consectetur. Mauris eget lacus lorem. Nulla tempus neque eu auctor dignissim.",
-                "p2": "Nulla pharetra erat in lectus suscipit dictum. Mauris venenatis lorem at ante aliquam lacinia. Etiam elementum lorem et lectus tempus pretium. Nunc ut feugiat leo, sed rutrum est. Praesent ac pretium turpis. Integer iaculis ligula magna, vitae commodo massa rhoncus ut. Suspendisse vestibulum sodales suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                "p3": "Cras ut mauris at risus pulvinar ultricies. Ut consectetur volutpat neque, in scelerisque felis pretium quis. Suspendisse potenti. Donec convallis auctor neque vitae pretium. Cras rhoncus vulputate quam, id ullamcorper ligula eleifend sit amet. Phasellus congue vulputate orci, viverra egestas ipsum sagittis a. Vestibulum diam mi, suscipit quis fermentum ac, vulputate ac magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-                "p4": "Nunc pretium tempus ligula quis bibendum. In hac habitasse platea dictumst. Aliquam interdum volutpat massa at accumsan. Nunc pulvinar vel libero vel dictum. Sed eget pellentesque mauris. Cras sodales, ante sed fermentum aliquam, leo quam pulvinar justo, quis laoreet leo ligula consectetur justo. Sed dictum feugiat nulla et vulputate."
+                "p1": "Me llamo Darío. Soy un desarrollador y estudiante de Ingeniería en Sistemas, oriundo de Buenos Aires. Me dedico a diseñar y programar soluciones que se ajusten a las necesidades de otros. Estoy interesado en trabajos full-time que tengan algunos días de home office y otros on-site, o que sean remotos con mucha comunicación.",
+                "p2": "Brindo valor a la empresa para la que trabajo, ocupándome de entender el dominio, modelando y documentando, analizando la mejor solución con mis pares, implementando, y realizando pruebas automatizadas: ahorrando tiempo y recursos tanto a la empresa como a mí. Compartir ideas me es importante, por eso considero que un trabajo basado en el intercambio de opiniones nos hace bien a todos. Trabajar con otras personas me permite aprender de ellos y mejorar.",
+                "p3": "Mi fuerte está en el backend, donde realizo modelos robustos que satisfacen las necesidades de hoy y escalan mañana. Java es mi principal lenguaje, haciendo uso de Spring, Spark, Hibernate, Gson y Handlebars. También sé usar JDBC, realizando queries contra (T- / PL- /- ) SQL. Proveo y consumo APIs REST en mis proyectos, aunque también he usado SOAP. Los paradigmas orientado a objetos y funcional, son los que más me gustan, combinando patrones de diseño con expresiones lambda en mi día a día.    ",
+                "p4": "Estoy aprendiendo de a poco front-end, y esta página es una excusa para seguir practicando. No contiene ningún framework porque me propuse aprender lo fundamental de CSS y mantenerme flexible. Me manejo bien en HTML5, CSS, Javascript, y puedo hacer uso de diversas librerías como jQuery, FontAwesome, i18next, y Handlebars para templating. Si querés ver cómo está hecho, te invito a pasar por ",
+                "alert-responsive": "Este sitio es responsive. ¡Abrilo donde quieras!",
+                "hey": "Hola!",
+                "repo":"mi repositorio."
             }
         }
     }
@@ -55,7 +61,7 @@ i18next.init(
     updateContent
 );
 
-var langCounter = 1;
+let langCounter = 1;
 const langs = ["en", "es"];
 
 document.getElementById("langButton").onclick = function(){
